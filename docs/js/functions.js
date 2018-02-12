df6ca75c526316dc0a288b05a8be49781b9d4916
Zettir <gerdes@outlook.dk>
2018-02-12 09:00:04 +0100

    Initial commit

---
+++ docs/js/functions.js
@@ -0,0 +1,16 @@
+/////////////////////////////
+// JavaScript | Functions //
+///////////////////////////
+
+
+function loadDocument(){
+
+//		let content = document.currentTarget.children("content");
+//		let subSection = document.querySelector(this).children.className = "subSection";
+//		let menu = document.querySelector(this).children.className = "menu";
+//		document.getElementsByClassName("to_be_moved").appendChild(menu+content);
+
+}
+
+//Load script when DOM is ready
+document.addEventListener("DOMContentLoaded",loadDocument);
